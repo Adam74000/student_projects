c77436e39644656a9407c937361ad3d8dd27e7e3
Adam74000 <student@student.machine>
2024-11-27 10:36:38 +0000

    studentmachine send

--- cpp/gra/main.cpp
+++ cpp/gra/main.cpp
@@ -3,11 +3,23 @@
 using namespace std;
 
 int main(){
-	cout<<"bond"<<endl;
-	int r = 0;
-	int k = 0;
-	char board[2][2];
-	for(int r=0; r<9;r++{
-		cout
-	}
+	char plansza[3][3] = { {' ', ' ', ' '}, {' ',' ', ' '}, {' ', ' ', ' '} };
+	int rzad,kolumna;
+	char gracz = 'x';
+	bool gratrwa = true;
+	int ruch = 0;
+
+	while (gratrwa) {
+		cout << endl;
+		for (int i = 0; i < 3; i++) {
+			for(int j = 0; j < 3; j++) {
+				cout << planasza[i][j] << " ";
+			}
+			cout << endl;
+		}
+		cout << "Gracz" << gracz << ", podaj wiersz (0-2) i kolumnę (0-2): ";
+		cin >> rzad >> kolumna;
+
+		if (plansza
 	return 0;
+}
