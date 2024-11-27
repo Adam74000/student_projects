1b6b105408f1355eb13f7034682dac1302c544ed
Adam74000 <student@student.machine>
2024-11-27 11:02:44 +0000

    studentmachine send

--- cpp/gra/main.cpp
+++ cpp/gra/main.cpp
@@ -20,6 +20,44 @@ int main(){
 		cout << "Gracz" << gracz << ", podaj wiersz (0-2) i kolumnę (0-2): ";
 		cin >> rzad >> kolumna;
 
-		if (plansza
+		if (plansza[rzad][kolumna] !=' ';{
+			cout << "To pole jest już zajęte! Spróbuj ponownie" << endl;
+
+			continue;
+		}
+
+		plansza[rzad][kolumna] = gracz;
+		ruchi++;
+
+		for (int i = 0; i < 3; i++){
+			if(plansza[i][0] == gracz && plansza[1][i] == gracz && plansza[i][2] == gracz)
+				gratrwa = false;
+				break;
+			}
+		}
+
+		for (int i = 0; i < 3; i++) {
+			if (plansza[0][i] == gracz && plansza[1][i] == gracz && plansza[2][i] == gracz)
+				gratrwa = false;
+				break;
+			}
+		}
+		if (plansza[0][0] == gracz && plansza[1][1] == gracz && plansza[2][2] == gracz) {
+			gratrwa = false;
+		}
+		if (plansza[0][2] == gracz && plansza[1][1] == gracz && plansza[2][0] == gracz){
+			gratrwa = false;
+		}
+		if (ruchi == 9) {
+			gratrwa = false;
+		}
+		if (gracz == 'x') {
+			gracz = '0';
+		} else {
+			gracz = 'x';
+		}
+	}
+
+
 	return 0;
 }
